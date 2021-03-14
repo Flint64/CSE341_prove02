92a2497d895a13722ecbc366b3372224f5cb38ac
Flint64 <twda96@gmail.com>
2021-03-13 18:01:51 -0700

    cors update

--- app.js
+++ app.js
@@ -22,6 +22,14 @@ app.set('views', 'views');
     app.use(bodyParser.urlencoded({extended: false}));
     
     app.use(express.static(path.join(__dirname, 'public')));
+
+    app.use((req, res, next) => {
+        // * = any, can be set to specific domains
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+        res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+        next();
+    });
     
     ////////////////////////////////////////////////////////////////////////////////////
     
